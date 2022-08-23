7d77446cfc4cbba4d38df8069572527ef8fd6b22
vanegabioudclear <vanesagabioud@yahoo.com.ar>
2022-08-23 17:39:17 -0300

    agregue funcionalidad al componente card

--- frontend/src/components/Card/Card.js
+++ frontend/src/components/Card/Card.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
 
-function Card() {
+
+function Card({name, id, img, price, stock, shortDesc}) {
+  name = "Celular"
+  stock= 5
+  price= 123
+  shortDesc= "Esta es una prueba para ver como queda"
   return (
-    <div>Card</div>
+    <div>
+      <h1>{name}</h1>
+      <img alt={name} src={img}/>
+      <h2>$ {price}</h2>
+      <h3>{stock} unidades</h3>
+      <p>{shortDesc}</p>
+    </div>
   )
 }
 
