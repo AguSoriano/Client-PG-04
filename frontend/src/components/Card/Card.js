d033d461719b700cda7c6ea726cc0b1470b60e0f
vanegabioudclear <vanesagabioud@yahoo.com.ar>
2022-08-23 20:41:41 -0300

    agregue style al component card

--- frontend/src/components/Card/Card.js
+++ frontend/src/components/Card/Card.js
@@ -1,20 +1,27 @@
-import React from 'react';
+import React from "react";
+import { Card } from "antd";
 
+const { Meta } = Card;
 
-function Card({name, id, img, price, stock, shortDesc}) {
-  name = "Celular"
-  stock= 5
-  price= 123
-  shortDesc= "Esta es una prueba para ver como queda"
+function CardP({ name, id, img, price, stock, shortDesc }) {
+  name = "Zapato";
+  img =
+    "https://i.pinimg.com/236x/70/a7/c7/70a7c7d1df887bbd70452f52aa462925--fashion-shoes-womens-fashion.jpg";
+  stock = 5;
+  price = 123;
+  shortDesc = "Esta es una prueba para ver como queda";
+  let priceM = `$ ${price}`;
   return (
     <div>
-      <h1>{name}</h1>
-      <img alt={name} src={img}/>
-      <h2>$ {price}</h2>
-      <h3>{stock} unidades</h3>
-      <p>{shortDesc}</p>
+      <Card
+        hoverable
+        style={{ width: 240 }}
+        cover={<img alt={name} src={img} />}
+      >
+        <Meta title={priceM} description={shortDesc} />
+      </Card>
     </div>
-  )
+  );
 }
 
-export default Card
+export default CardP;
