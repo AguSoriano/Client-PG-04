ec2e8038b45d505701863dcd8eb994856d16aa7e
vanegabioudclear <vanesagabioud@yahoo.com.ar>
2022-08-25 12:55:07 -0300

    creado el detalle de produc en el reducer

--- frontend/src/redux/reducer/index.js
+++ frontend/src/redux/reducer/index.js
@@ -1,72 +1,74 @@
 import {
-
-    GET_NAME_PRODUCT,
-    GET_PRODUCTS,
-    ORDER_ALPHABETICAL,
-    ORDER_PRICE
-
+  GET_NAME_PRODUCT,
+  GET_PRODUCTS,
+  ORDER_ALPHABETICAL,
+  ORDER_PRICE,
 } from "../actions/ActionsTypes";
-
+import { GET_DETAIL } from "../actions/ActionTypes";
 
 const initialState = {
-    product: [],
-}
+  product: [],
+  prodDetail: [],
+};
 
 export default function reducer(state = initialState, { type, payload }) {
-    //vamos a ejecutar los typos de accion para saber donde ejecutar cada logica
-    switch (type) {
+  //vamos a ejecutar los typos de accion para saber donde ejecutar cada logica
+  switch (type) {
+    case GET_NAME_PRODUCT:
+      return {
+        ...state,
+        product: payload,
+      };
 
-        case GET_NAME_PRODUCT:
-            return {
-                ...state,
-                product: payload
-            }
+    case GET_PRODUCTS:
+      return {
+        ...state,
+        product: payload,
+      };
 
-        case GET_PRODUCTS:
-            return {
-                ...state,
-                product: payload,
+    case ORDER_ALPHABETICAL:
+      const sortedName =
+        payload === "A-Z"
+          ? state.product.sort((a, b) => {
+              if (a.name > b.name) {
+                return 1;
+              }
+              if (b.name > a.name) {
+                return -1;
+              }
+              return 0;
+            })
+          : state.product.sort((a, b) => {
+              if (a.name > b.name) {
+                return -1;
+              }
+              if (b.name > a.name) {
+                return 1;
+              }
+              return 0;
+            });
+      return {
+        ...state,
+        product: sortedName,
+      };
 
-            }
-            
-        case ORDER_ALPHABETICAL:
-
-            const sortedName =
-                payload === "A-Z"
-                    ? state.product.sort((a, b) => {
-                        if (a.name > b.name) {
-                            return 1;
-                        }
-                        if (b.name > a.name) {
-                            return -1;
-                        }
-                        return 0;
-                    })
-                    : state.product.sort((a, b) => {
-                        if (a.name > b.name) {
-                            return -1;
-                        }
-                        if (b.name > a.name) {
-                            return 1;
-                        }
-                        return 0;
-                    });
-            return {
-                ...state,
-                product: sortedName,
-
-            };
-
-            case ORDER_PRICE:
-            const sortedPrice =
-                payload === "min_price"
-                    ? state.product.sort((a, b) => parseInt(a.price) - parseInt(b.price))
-                    : state.product.sort((a, b) => parseInt(b.price) - parseInt(a.price))
-            return {
-                ...state,
-                product: sortedPrice,
-            };
-
-        default: return state
+    case ORDER_PRICE:
+      const sortedPrice =
+        payload === "min_price"
+          ? state.product.sort((a, b) => parseInt(a.price) - parseInt(b.price))
+          : state.product.sort((a, b) => parseInt(b.price) - parseInt(a.price));
+      return {
+        ...state,
+        product: sortedPrice,
+      };
+    case GET_DETAIL: {
+      return {
+        ...state,
+        prodDetail: payload,
+      };
     }
+
+    default:
+      return state;
+  }
 }
