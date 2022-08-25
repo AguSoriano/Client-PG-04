05f183eda2ee59455bf163e60f4f4debb5efc56b
vanegabioudclear <vanesagabioud@yahoo.com.ar>
2022-08-25 12:43:20 -0300

    creado action para detalle

--- frontend/src/redux/actions/ActionTypes.js
+++ frontend/src/redux/actions/ActionTypes.js
@@ -1,4 +1,5 @@
 export const GET_NAME_PRODUCT = "GET_NAME_PRODUCT";
 export const GET_PRODUCTS = "GET_PRODUCTS";
 export const ORDER_ALPHABETICAL = "ORDER_ALPHABETICAL";
-export const ORDER_PRICE= "ORDER_PRICE";
+export const ORDER_PRICE = "ORDER_PRICE";
+export const GET_DETAIL = "GET_DETAIL";
