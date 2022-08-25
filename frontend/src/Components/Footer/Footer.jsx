0e93217c3d7d3d19461b70dfb88d51fdf328fe54
Agustin <agustin.sorianoo@gmail.com>
2022-08-25 20:29:18 -0300

    cree componente Footer

---
+++ frontend/src/Components/Footer/Footer.jsx
@@ -0,0 +1,90 @@
+import React from 'react';
+
+function Footer() {
+    let fecha = new Date().getFullYear();
+
+    return (
+        <footer>
+            <div>
+                <div>
+                    <p>
+                        Page created by Group_04
+                    </p>
+                    <p>&copy; {fecha}. All Rights Reserved.</p>
+                </div>
+                <div>
+                    <a href="https://www.linkedin.com/in/federico-eloy-fucci-87664357/" target="_blank" rel="noopener noreferrer">
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg" alt="linkedin" />
+                        <h4>Federico Eloy Fucci</h4>
+                    </a>
+                    <a href="https://github.com/federicofucci21" target="_blank" rel="noopener noreferrer">
+                        <img src="https://icon-icons.com/es/icono/github-logo/147285" alt="gitHub" />
+                    </a>
+                </div>
+                <div>
+                    <a href="https://www.linkedin.com/in/mario-a-caballero/" target="_blank" rel="noopener noreferrer">
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg" alt="linkedin" />
+                        <h4>Mario Caballero</h4>
+                    </a>
+                    <a href="https://github.com/marioacaballero" target="_blank" rel="noopener noreferrer">
+                        <img src="https://icon-icons.com/es/icono/github-logo/147285" alt="gitHub" />
+                    </a>
+                </div>
+                <div>
+                    <a href="https://www.linkedin.com/in/sofia-gonzalez-zapiola/" target="_blank" rel="noopener noreferrer">
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg" alt="linkedin" />
+                        <h4>Sofia Gonzalez</h4>
+                    </a>
+                    <a href="https://github.com/ChofGonzalez" target="_blank" rel="noopener noreferrer">
+                        <img src="https://icon-icons.com/es/icono/github-logo/147285" alt="gitHub" />
+                    </a>
+                </div>
+                <div>
+                    <a href="https://github.com/vanegabioud" target="_blank" rel="noopener noreferrer">
+                        <img src="https://icon-icons.com/es/icono/github-logo/147285" alt="gitHub" />
+                        <h4>Vanesa Gabioud</h4>
+                    </a>
+                </div>
+                <div>
+                    <a href="https://www.linkedin.com/in/hidalgoeduardo/" target="_blank" rel="noopener noreferrer">
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg" alt="linkedin" />
+                        <h4>Eduardo Hidalgo</h4>
+                    </a>
+                    <a href="https://github.com/hidalgo86" target="_blank" rel="noopener noreferrer">
+                        <img src="https://icon-icons.com/es/icono/github-logo/147285" alt="gitHub" />
+                    </a>
+                </div>
+                <div>
+                    <a href="https://www.linkedin.com/in/cperlovan/" target="_blank" rel="noopener noreferrer">
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg" alt="linkedin" />
+                        <h4>Carlos Perez</h4>
+                    </a>
+                    <a href="https://github.com/cperlovan" target="_blank" rel="noopener noreferrer">
+                        <img src="https://icon-icons.com/es/icono/github-logo/147285" alt="gitHub" />
+                    </a>
+                </div>
+                <div>
+                    <a href="https://www.linkedin.com/in/luciacostamagna/" target="_blank" rel="noopener noreferrer">
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg" alt="linkedin" />
+                        <h4>Lucia Costamagna</h4>
+                    </a>
+                    <a href="https://github.com/Lucostamagna" target="_blank" rel="noopener noreferrer">
+                        <img src="https://icon-icons.com/es/icono/github-logo/147285" alt="gitHub" />
+                    </a>
+                </div>
+                <div>
+                    <a href="https://www.linkedin.com/in/agustin-soriano-027876119/" target="_blank" rel="noopener noreferrer">
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg" alt="linkedin" />
+                        <h4>Agustin Soriano</h4>
+                    </a>
+                    <a href="https://github.com/AguSoriano" target="_blank" rel="noopener noreferrer">
+                        <img src="https://icon-icons.com/es/icono/github-logo/147285" alt="gitHub" />
+                    </a>
+
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
