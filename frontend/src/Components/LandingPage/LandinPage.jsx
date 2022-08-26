b6cc2ea4f2b4190e271d4bd7533c316f36214d0f
unknown <luci3220@hotmail.com>
2022-08-25 22:00:24 -0300

    rutes

---
+++ frontend/src/Components/LandingPage/LandinPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function LandingPage () {
+    return(
+        <div> 
+            Hello World 
+             SOY UNA FUTURA LANDING PAGE 
+         </div>
+    )
+}
+export default LandingPage;
