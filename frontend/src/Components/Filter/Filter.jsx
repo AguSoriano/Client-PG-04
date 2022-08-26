3070e1cf0db5e95daf108d99c941bb4bbbc5ae1d
unknown <luci3220@hotmail.com>
2022-08-26 20:38:36 -0300

    filters

---
+++ frontend/src/Components/Filter/Filter.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+
+function Filter (){ 
+    return (
+        <div>
+        <p> filtrar productos</p>
+        </div>
+    )
+}
+
+export default Filter
---
+++ frontend/src/Components/Sort/Sort.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Sort (){
+    return(
+        <div>
+            <p> ordenamiento </p>
+        </div>
+    )
+}
+
+export default Sort;
