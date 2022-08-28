7fb7563be5d1c28ee670277bf0a6b13ce3f91957
Mario <mario.caballero.948@gmail.com>
2022-08-28 19:44:17 -0300

    fix actions rutes and home

--- frontend/src/redux/actions/index.jsx
+++ frontend/src/redux/actions/index.jsx
@@ -1,99 +1,99 @@
 import axios from "axios";
 import {
-    GET_NAME_PRODUCT,
-    GET_PRODUCTS,
-    ORDER_ALPHABETICAL,
-    ORDER_PRICE,
-    GET_PRODUCT_BY_CATEGORY,
-    GET_CATEGORIES
+  GET_NAME_PRODUCT,
+  GET_PRODUCTS,
+  ORDER_ALPHABETICAL,
+  ORDER_PRICE,
+  GET_PRODUCT_BY_CATEGORY,
+  GET_CATEGORIES,
 } from "./ActionTypes";
 import { GET_DETAIL } from "./ActionTypes";
-import { products } from "../../DB/db";
+// import { products } from "../../DB/db";
 
 export function getNameProduct(name) {
-    return async function (dispatch) {
-        try {
-            const json = await axios.get(
-                `{aca dentro va el path de busqueda por nombre}${name}`
-            );
-            return dispatch({
-                type: GET_NAME_PRODUCT,
-                payload: json.data,
-            });
-        } catch (error) {
-            return error;
-        }
-    };
+  return async function (dispatch) {
+    try {
+      const json = await axios.get(
+        `https://pf-api-04.up.railway.app/products${name}`
+      );
+      return dispatch({
+        type: GET_NAME_PRODUCT,
+        payload: json.data,
+      });
+    } catch (error) {
+      return error;
+    }
+  };
 }
 
 export function getProducts() {
-    return async function (dispatch) {
-        try {
-            // let json = await axios.get("path get/products", {});
+  return async function (dispatch) {
+    try {
+      let json = await axios.get("https://pf-api-04.up.railway.app/products");
 
-            return dispatch({
-                type: GET_PRODUCTS,
-                // payload: json.data,
-                payload: products,
-            });
-        } catch (error) {
-            return error;
-        }
-    };
+      return dispatch({
+        type: GET_PRODUCTS,
+        payload: json.data,
+        // payload: products,
+      });
+    } catch (error) {
+      return error;
+    }
+  };
 }
 
 export function getCategories() {
-    return function (dispatch) {
-        return axios.get("path/products/category")
-            .then(response => {
-                dispatch({ type: GET_CATEGORIES, payload: response.data });
-            })
-
-    };
-
+  return function (dispatch) {
+    return axios
+      .get("https://pf-api-04.up.railway.app/category")
+      .then((response) => {
+        dispatch({ type: GET_CATEGORIES, payload: response.data });
+      });
+  };
 }
 
 export function getProductByCategory(categoryName) {
-    return function (dispatch) {
-        return axios.get(`/products/category/${categoryName}`)
-            .then(products => {
-                dispatch({ type: GET_PRODUCT_BY_CATEGORY, payload: products.data });
-            })
-            .catch(err => console.log(err))
-    };
+  return function (dispatch) {
+    return axios
+      .get(`/products/category/${categoryName}`)
+      .then((products) => {
+        dispatch({ type: GET_PRODUCT_BY_CATEGORY, payload: products.data });
+      })
+      .catch((err) => console.log(err));
+  };
 }
 
 export function OrderAlphabetical(payload) {
-    try {
-        return {
-            type: ORDER_ALPHABETICAL,
-            payload,
-        };
-    } catch (error) {
-        return error;
-    }
+  try {
+    return {
+      type: ORDER_ALPHABETICAL,
+      payload,
+    };
+  } catch (error) {
+    return error;
+  }
 }
 
 export function OrderPrice(payload) {
-    return {
-        type: ORDER_PRICE,
-        payload,
-    };
+  return {
+    type: ORDER_PRICE,
+    payload,
+  };
 }
 
-export const getDetail = () => {
-    return async (dispatch) => {
-        try {
-            const product = await axios.get("aca va la ruta");
+export const getDetail = (id) => {
+  return async (dispatch) => {
+    try {
+      const product = await axios.get(
+        `https://pf-api-04.up.railway.app/products/${id}`
+      );
 
-            return dispatch({
-                type: GET_DETAIL,
-                payload: product.data,
-            });
-        } catch (error) {
-            console.log(error);
-        }
-    };
+      return dispatch({
+        type: GET_DETAIL,
+        payload: product.data,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 };
-
-
