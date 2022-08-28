c09bb2ccdfeaa1c286c19298ddbd87a2f3fdbadc
Agustín Soriano <97198911+AguSoriano@users.noreply.github.com>
2022-08-28 13:10:04 -0300

    Revert "Footer"

--- frontend/src/redux/actions/index.jsx
+++ frontend/src/redux/actions/index.jsx
@@ -1,56 +1,44 @@
 import axios from "axios";
 import {
-    GET_NAME_PRODUCT,
-    GET_PRODUCTS,
-    ORDER_ALPHABETICAL,
-    ORDER_PRICE,
-    GET_PRODUCT_BY_CATEGORY,
-    GET_CATEGORIES
+  GET_NAME_PRODUCT,
+  GET_PRODUCTS,
+  ORDER_ALPHABETICAL,
+  ORDER_PRICE,
+  GET_PRODUCT_BY_CATEGORY
 } from "./ActionTypes";
 import { GET_DETAIL } from "./ActionTypes";
 import { products } from "../../DB/db";
 
 export function getNameProduct(name) {
-    return async function (dispatch) {
-        try {
-            const json = await axios.get(
-                `{aca dentro va el path de busqueda por nombre}${name}`
-            );
-            return dispatch({
-                type: GET_NAME_PRODUCT,
-                payload: json.data,
-            });
-        } catch (error) {
-            return error;
-        }
-    };
+  return async function (dispatch) {
+    try {
+      const json = await axios.get(
+        `{aca dentro va el path de busqueda por nombre}${name}`
+      );
+      return dispatch({
+        type: GET_NAME_PRODUCT,
+        payload: json.data,
+      });
+    } catch (error) {
+      return error;
+    }
+  };
 }
 
 export function getProducts() {
-    return async function (dispatch) {
-        try {
-            // let json = await axios.get("path get/products", {});
+  return async function (dispatch) {
+    try {
+      // let json = await axios.get("path get/products", {});
 
-            return dispatch({
-                type: GET_PRODUCTS,
-                // payload: json.data,
-                payload: products,
-            });
-        } catch (error) {
-            return error;
-        }
-    };
-}
-
-export function getCategories() {
-    return function (dispatch) {
-        return axios.get("path/products/category")
-            .then(response => {
-                dispatch({ type: GET_CATEGORIES, payload: response.data });
-            })
-
-    };
-
+      return dispatch({
+        type: GET_PRODUCTS,
+        // payload: json.data,
+        payload: products,
+      });
+    } catch (error) {
+      return error;
+    }
+  };
 }
 
 export function getProductByCategory(categoryName) {
@@ -64,36 +52,36 @@ export function getProductByCategory(categoryName) {
 }
 
 export function OrderAlphabetical(payload) {
-    try {
-        return {
-            type: ORDER_ALPHABETICAL,
-            payload,
-        };
-    } catch (error) {
-        return error;
-    }
+  try {
+    return {
+      type: ORDER_ALPHABETICAL,
+      payload,
+    };
+  } catch (error) {
+    return error;
+  }
 }
 
 export function OrderPrice(payload) {
-    return {
-        type: ORDER_PRICE,
-        payload,
-    };
+  return {
+    type: ORDER_PRICE,
+    payload,
+  };
 }
 
 export const getDetail = () => {
-    return async (dispatch) => {
-        try {
-            const product = await axios.get("aca va la ruta");
+  return async (dispatch) => {
+    try {
+      const product = await axios.get("aca va la ruta");
 
-            return dispatch({
-                type: GET_DETAIL,
-                payload: product.data,
-            });
-        } catch (error) {
-            console.log(error);
-        }
-    };
+      return dispatch({
+        type: GET_DETAIL,
+        payload: product.data,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 };
 
 
