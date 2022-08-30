4cd5526b90cb92aeb7edd6dff210c36f38e8b654
Mario <mario.caballero.948@gmail.com>
2022-08-30 12:12:26 -0300

    add week products

--- frontend/src/redux/actions/ActionTypes.js
+++ frontend/src/redux/actions/ActionTypes.js
@@ -8,3 +8,4 @@ export const GET_CATEGORIES = "GET_CATEGORIES";
 export const CLEAN_DETAIL = "CLEAN_DETAIL";
 export const FILTER_BY = "FILTER_BY";
 export const SET_PAGE = "SET_PAGE";
+export const WEEK_PROD = "WEEK_PROD";
