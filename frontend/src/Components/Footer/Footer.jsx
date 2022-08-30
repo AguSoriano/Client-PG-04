f7aae6f4ff8b1b011b3eca2d48ce70527184b91e
Agustin <agustin.sorianoo@gmail.com>
2022-08-30 20:28:58 -0300

    estilos-footer

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -6,13 +6,14 @@ function Footer() {
 
     return (
         <footer className={style.footer}>
-            <div className={style.site_footer}>
-                <div className={style.copyright}>
+            <div className={style.copyright}>
                     <p>
                         Page created by Group_04
                     </p>
                     <p>&copy; {fecha}. All Rights Reserved.</p>
                 </div>
+            <div className={style.site_footer}>
+                
                 <div className={style.redes_sociales}>
                     <div>
                     <a href="https://www.linkedin.com/in/federico-eloy-fucci-87664357/" target="_blank" rel="noopener noreferrer">
@@ -21,7 +22,7 @@ function Footer() {
                     <a href="https://github.com/federicofucci21" target="_blank" rel="noopener noreferrer">
                         <img src="https://play-lh.googleusercontent.com/PCpXdqvUWfCW1mXhH1Y_98yBpgsWxuTSTofy3NGMo9yBTATDyzVkqU580bfSln50bFU" alt="gitHub" />
                     </a>
-                    <h4>Federico Eloy Fucci</h4>
+                    <h4 className={style.nombre}>Federico Eloy Fucci</h4>
                     </div>
                 </div>
                 <div className={style.redes_sociales}>
@@ -33,7 +34,7 @@ function Footer() {
                     <a href="https://github.com/marioacaballero" target="_blank" rel="noopener noreferrer">
                         <img src="https://play-lh.googleusercontent.com/PCpXdqvUWfCW1mXhH1Y_98yBpgsWxuTSTofy3NGMo9yBTATDyzVkqU580bfSln50bFU" alt="gitHub" />
                     </a>
-                    <h4>Mario Caballero</h4>
+                    <h4 className={style.nombre}>Mario Caballero</h4>
                     </div>
                 </div>
                 <div className={style.redes_sociales}>
@@ -45,14 +46,14 @@ function Footer() {
                     <a href="https://github.com/ChofGonzalez" target="_blank" rel="noopener noreferrer">
                         <img src="https://play-lh.googleusercontent.com/PCpXdqvUWfCW1mXhH1Y_98yBpgsWxuTSTofy3NGMo9yBTATDyzVkqU580bfSln50bFU" alt="gitHub" />
                     </a>
-                    <h4>Sofia Gonzalez</h4>
+                    <h4 className={style.nombre}>Sofia Gonzalez</h4>
                     </div>
                 </div>
                 <div className={style.redes_sociales}>
                     <div></div>
                     <a href="https://github.com/vanegabioud" target="_blank" rel="noopener noreferrer">
                         <img src="https://play-lh.googleusercontent.com/PCpXdqvUWfCW1mXhH1Y_98yBpgsWxuTSTofy3NGMo9yBTATDyzVkqU580bfSln50bFU" alt="gitHub" />
-                        <h4>Vanesa Gabioud</h4>
+                        <h4 className={style.nombre}>Vanesa Gabioud</h4>
                     </a>
                 </div>
                 <div className={style.redes_sociales}>
@@ -64,7 +65,7 @@ function Footer() {
                     <a href="https://github.com/hidalgo86" target="_blank" rel="noopener noreferrer">
                         <img src="https://play-lh.googleusercontent.com/PCpXdqvUWfCW1mXhH1Y_98yBpgsWxuTSTofy3NGMo9yBTATDyzVkqU580bfSln50bFU" alt="gitHub" />
                     </a>
-                    <h4>Eduardo Hidalgo</h4>
+                    <h4 className={style.nombre}>Eduardo Hidalgo</h4>
                     </div>
                 </div>
                 <div className={style.redes_sociales}>
@@ -76,7 +77,7 @@ function Footer() {
                     <a href="https://github.com/cperlovan" target="_blank" rel="noopener noreferrer">
                         <img src="https://play-lh.googleusercontent.com/PCpXdqvUWfCW1mXhH1Y_98yBpgsWxuTSTofy3NGMo9yBTATDyzVkqU580bfSln50bFU" alt="gitHub" />
                     </a>
-                    <h4>Carlos Perez</h4>
+                    <h4 className={style.nombre}>Carlos Perez</h4>
                     </div>
                 </div>
                 <div className={style.redes_sociales}>
@@ -88,7 +89,7 @@ function Footer() {
                     <a href="https://github.com/Lucostamagna" target="_blank" rel="noopener noreferrer">
                         <img src="https://play-lh.googleusercontent.com/PCpXdqvUWfCW1mXhH1Y_98yBpgsWxuTSTofy3NGMo9yBTATDyzVkqU580bfSln50bFU" alt="gitHub" />
                     </a>
-                    <h4>Lucia Costamagna</h4>
+                    <h4 className={style.nombre}>Lucia Costamagna</h4>
                     </div>
                 </div >
                 <div className={style.redes_sociales}>
@@ -100,7 +101,7 @@ function Footer() {
                     <a href="https://github.com/AguSoriano" target="_blank" rel="noopener noreferrer">
                         <img src="https://play-lh.googleusercontent.com/PCpXdqvUWfCW1mXhH1Y_98yBpgsWxuTSTofy3NGMo9yBTATDyzVkqU580bfSln50bFU" alt="gitHub" />
                     </a>
-                    <h4>Agustin Soriano</h4>
+                    <h4 className={style.nombre}>Agustin Soriano</h4>
                     </div>
 
                 </div>
