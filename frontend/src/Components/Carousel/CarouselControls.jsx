7ffce50866e871020f92427b1efdab5402f681c9
Agustin <agustin.sorianoo@gmail.com>
2022-09-01 19:15:13 -0300

    modifique logica completa de carousel

---
+++ frontend/src/Components/Carousel/CarouselControls.jsx
@@ -0,0 +1,10 @@
+const CarouselControls = ({ prev, next }) => {
+    return (
+        <div>
+            <button className="carousel-control left" onClick={prev}>Prev</button>
+            <button className="carousel-control right" onClick={next}>Next</button>
+        </div>
+    )
+}
+
+export default CarouselControls
--- frontend/src/Components/LandingPage/LandinPage.jsx
+++ frontend/src/Components/LandingPage/LandinPage.jsx
@@ -1,16 +1,36 @@
 import React from "react";
-import Carousel from "../Carousel/Corousel"
+import Carousel from "../Carousel/Carousel"
 
 
 function LandingPage() {
+
+    const slides = [
+        {
+            src: require('../Img/PG0.png'),
+          },
+          {
+            src: require('../Img/PG1.jpeg'),
+          },
+          {
+            src: require('../Img/PG2.jpeg'),
+          },
+          {
+            src: require('../Img/PG3.jpeg'),
+          },
+          {
+            src: require('../Img/PG4.jpg'),
+          },
+          {
+            src: require('../Img/PG5.jpg'),
+          }
+      ]
+
     return (
         <div>
-            <div padding="50px">
-                <Carousel />
+            <div>
+            <Carousel slides={slides} controls indicators width={1200} />
             </div>
         </div>
-
-
     )
 }
 export default LandingPage;
