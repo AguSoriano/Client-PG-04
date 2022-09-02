71ed0b43eac00c5a87d28e5ef2fcb0cecfeffde0
Mario <mario.caballero.948@gmail.com>
2022-09-02 19:36:49 -0300

    add login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,25 @@
-import './App.css';
+import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import NavBar from "./Components/NavBar/NavBar";
 import Detail from "./Components/Detail/Detail";
-import LandingPage from './Components/LandingPage/LandinPage';
-import Create from './Components/Create/Create';
-
-
+import LandingPage from "./Components/LandingPage/LandinPage";
+import Create from "./Components/Create/Create";
+import Profile from "./Components/Profile/Profile";
 
 function App() {
   return (
     <div className="App">
-     
-     <NavBar path="/" element={NavBar} />
+      <NavBar path="/" element={NavBar} />
 
       <Routes>
         <Route exact path="/" element={<LandingPage />} />
         <Route exact path="home" element={<Home />} />
         <Route exact path="products/:id" element={<Detail />} />
-        <Route  path="new" element={<Create />} />
+        <Route path="new" element={<Create />} />
+        <Route path="profile" element={<Profile />} />
       </Routes>
     </div>
-      
   );
 }
 
