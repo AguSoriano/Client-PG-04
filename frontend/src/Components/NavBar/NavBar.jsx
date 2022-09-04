530268066dc8aa0c73d10707fbcadf624f247f6b
Mario <mario.caballero.948@gmail.com>
2022-09-04 12:23:36 -0300

    fix profile

--- frontend/src/Components/NavBar/NavBar.jsx
+++ frontend/src/Components/NavBar/NavBar.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import SearchBar from "../SearchBar/searchBar";
 import style from "./NavBar.module.css";
 import { FaHeart, FaCartArrowDown } from "react-icons/fa";
 import { AiOutlineTeam } from "react-icons/ai";
 import { useAuth0 } from "@auth0/auth0-react";
+import * as ReactRedux from "react-redux";
+import { register } from "../../redux/actions";
 
 function NavBar() {
-  const { loginWithRedirect } = useAuth0();
+  const dispatch = ReactRedux.useDispatch();
+  const { user, loginWithRedirect, isAuthenticated } = useAuth0();
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      dispatch(register(user));
+    }
+  }, [isAuthenticated]);
 
   return (
     <div className={style.navBar}>
@@ -37,7 +46,9 @@ function NavBar() {
           <FaCartArrowDown size="1.5rem" />
         </Link>
         <Link to="/profile">PERFIL</Link>
-        <Link className={style.link} to={"/about"}>Sobre Nosotros</Link>
+        <Link className={style.link} to={"/about"}>
+          Sobre Nosotros
+        </Link>
         {/* <Link className={style.link} to={"/"}>Post</Link> */}
       </section>
     </div>
