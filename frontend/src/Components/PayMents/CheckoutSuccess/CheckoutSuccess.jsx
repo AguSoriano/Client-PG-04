88254dbfb4157aebf51ea3e54c2c63e1dc49b897
hidalgo86 <95661987+hidalgo86@users.noreply.github.com>
2022-09-12 18:12:30 -0300

    checkout a contact

--- frontend/src/Components/PayMents/CheckoutSuccess/CheckoutSuccess.jsx
+++ frontend/src/Components/PayMents/CheckoutSuccess/CheckoutSuccess.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import styles from "./CheckoutSuccess.module.css";
-import pagoexitoso from "../../../imagenes/pagoexitoso.jpg"
+import pagoexitoso from "../../../imagenes/pagoexitoso.jpg";
+import { Link } from "react-router-dom";
 
 export default function AppPago({ userId }) {
   return (
-  <div className={styles.container}>
-    <img className={styles.img}  src={pagoexitoso} alt="pago exitoso" />
-    <h1>Pago Exitoso</h1>
-  </div>
-)}
+    <div className={styles.container}>
+      <img className={styles.img} src={pagoexitoso} alt="pago exitoso" />
+      <h1>Pago Exitosoo</h1>
+      <Link className={styles.link} to={"/products/contact"}>
+        <button className={styles.button1}> Aceptar </button>
+      </Link>
+    </div>
+  );
+}
