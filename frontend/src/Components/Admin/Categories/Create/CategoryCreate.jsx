435fee4153924ab0898551272f13ea00d2be5126
Mario <mario.caballero.948@gmail.com>
2022-09-12 18:02:43 -0300

    start with category action to create

--- frontend/src/Components/Admin/Categories/Create/CategoryCreate.jsx
+++ frontend/src/Components/Admin/Categories/Create/CategoryCreate.jsx
@@ -1,17 +1,30 @@
 import React, { useState } from "react";
-import style from "./CategoryCreate.module.css"
+import style from "./CategoryCreate.module.css";
+import * as ReactRedux from "react-redux";
+import { createCategory } from "../../../../redux/actions/Categories/CategoryAction";
 
 function CategoryCreate() {
+  const dispatch = ReactRedux.useDispatch();
   const [input, setInput] = useState({
     name: "",
   });
 
   const handleInputChange = (e) => {
     e.preventDefault();
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    // dispatch(createCategory(input))
+    console.log(input);
+    alert(`La categoria ${input.name}`);
+    setInput({
+      name: "",
+    });
   };
   return (
     <form
@@ -31,6 +44,7 @@ function CategoryCreate() {
           />
           {/* {errors.name && <p className={style.errors}>{errors.name}</p>} */}
         </section>
+        <button type="submit">Crear</button>
       </div>
     </form>
   );
