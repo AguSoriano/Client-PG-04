a3182d87c33519fdc27ddc14f5827401cb438c59
hidalgo86 <95661987+hidalgo86@users.noreply.github.com>
2022-09-14 19:53:53 -0300

    ajustes de la pasarela de pago

--- frontend/src/Components/PayMents/PaymentCreate/PaymentCreate.jsx
+++ frontend/src/Components/PayMents/PaymentCreate/PaymentCreate.jsx
@@ -5,25 +5,22 @@ import { useAuth0 } from "@auth0/auth0-react";
 import CheckoutForm from "../CheckoutForm/CheckoutForm";
 import axios from "axios";
 import styles from "./PaymentCreate.module.css";
-import { BiUser } from "react-icons/bi";
 
 //clave publica
 const stripePromise = loadStripe(
   "pk_test_51LeL9JItJw2qRdRyTqjWxMEj6dNHj5ytf10BX8BxSmB1sjwqUeDZNhY2E7p6pKGYsn1XVvZeyyxPywpV6lUApH7200x4hSwa81"
 );
 
-export default function PaymentCreate({ userId }) {
-  const { user, isAuthenticated, loginWithRedirect } = useAuth0();
-
+export default function PaymentCreate({ user, productId }) {
   const [clientSecret, setClientSecret] = useState("");
 
   useEffect(() => {
     axios
-      .post("http://localhost:3001/create-payment-intent", {
-        userId: user.email,
+      .post("https://pf-api-04.up.railway.app/payment/create-payment-intent", {
+        userId: user,
       })
       .then((res) => setClientSecret(res.data.clientSecret));
-  }, [user]);
+  },[]);
 
   const options = {
     clientSecret,
@@ -31,18 +28,10 @@ export default function PaymentCreate({ userId }) {
 
   return (
     <div className={styles.container}>
-      {isAuthenticated && clientSecret ? (
+      {clientSecret && (
         <Elements options={options} stripe={stripePromise}>
           <CheckoutForm />
         </Elements>
-      ) : (
-        <button
-            onClick={() => loginWithRedirect()}
-            style={{ background: "none", border: "none", cursor: "pointer" }}
-          >
-            <BiUser size="1.5rem" />
-            Ingresar
-          </button>
       )}
     </div>
   );
